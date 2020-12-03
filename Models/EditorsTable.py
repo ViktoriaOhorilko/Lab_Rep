68260bc955392ae14803330d638b87c3c72c4445
BodiaPavlyk <bogdan2002mih@gmail.com>
2020-12-03 03:02:03 +0200

    Initial commit

--- Models/EditorsTable.py
+++
@@ -1,6 +0,0 @@
-from db import db
-
-editors = db.Table('editor',
-                  db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-                  db.Column('note_id', db.Integer, db.ForeignKey('note.id'))
-                  )
